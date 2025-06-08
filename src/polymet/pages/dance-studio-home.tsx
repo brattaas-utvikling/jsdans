da260b2d0f77b0b4715ee532372791059803a957
h-bcode <jon.are.br@gmail.com>
2025-06-08 13:02:20 +0200

    removed courses from homepage

--- src/polymet/pages/dance-studio-home.tsx
+++ src/polymet/pages/dance-studio-home.tsx
@@ -3,7 +3,6 @@ import AboutSection from "@/polymet/components/about-section";
 import ClassesSection from "@/polymet/components/classes-section";
 import ContactSection from "@/polymet/components/contact-section";
 // import { DanceClassCard } from "@/components/DanceClassCard";
-import CoursesPage from "@/components/CoursesPage";
 // import LoginForm from "@/components/LoginForm";
 // import DanceSchedule from "../components/dance-scedule";
 
@@ -12,15 +11,6 @@ export default function DanceStudioHome() {
     <>
       <HeroSection />
 
-      <CoursesPage
-        danceClasses={[]}
-        schedules={[]}
-        packages={[]}
-        onAddToCart={() => {}}
-        cartItems={[]}
-        onRemoveFromCart={() => {}}
-        onCheckout={() => {}}
-      />
       
       <AboutSection />
 
