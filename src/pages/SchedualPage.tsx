013bac317bd7efc8a545192c67d2e3780757b465
Jon Are <jon.are.br@gmail.com>
2025-08-31 22:59:34 +0200

    fix: update schedule date to 31. august 2025 in SchedualPage component

--- src/pages/SchedualPage.tsx
+++ src/pages/SchedualPage.tsx
@@ -635,7 +635,7 @@ const heightInPx = isDesktop
             </Button>
           </div>
           <div className="text-center mt-6 text-gray-500 dark:text-gray-400 font-montserrat text-xs">
-            Timeplanen er oppdatert 25. august 2025. Forbehold om endringer.
+            Timeplanen er oppdatert 31. august 2025. Forbehold om endringer.
           </div>
         </div>
       </section>
